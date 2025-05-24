eda85c00d14713a4f7c122b430497404d71b736c
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-05-24 19:03:54 +0100

    fix(deps): update terraform terraform-ibm-modules/cloud-monitoring/ibm to v1.2.10 (#46)

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -14,7 +14,7 @@ module "resource_group" {
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version           = "1.2.9"
+  version           = "1.2.10"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
   resource_tags     = var.resource_tags
