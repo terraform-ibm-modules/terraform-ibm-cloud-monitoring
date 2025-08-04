b85574a89a68e60d976e3cf848b2fba9837270de
shemau <shemau@gmail.com>
2025-08-04 14:21:48 +0100

    fix(deps): Bump dependencies

--- README.md
+++ README.md
@@ -146,13 +146,13 @@ You need the following permissions to run this module.
 | Name | Version |
 |------|---------|
 | <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.9.0 |
-| <a name="requirement_ibm"></a> [ibm](#requirement\_ibm) | >= 1.76.1, < 2.0.0 |
+| <a name="requirement_ibm"></a> [ibm](#requirement\_ibm) | >= 1.79.0, < 2.0.0 |
 
 ### Modules
 
 | Name | Source | Version |
 |------|--------|---------|
-| <a name="module_cbr_rule"></a> [cbr\_rule](#module\_cbr\_rule) | terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module | 1.31.0 |
+| <a name="module_cbr_rule"></a> [cbr\_rule](#module\_cbr\_rule) | terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module | 1.32.6 |
 
 ### Resources
 
--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -4,17 +4,17 @@
 
 module "resource_group" {
   source              = "terraform-ibm-modules/resource-group/ibm"
-  version             = "1.2.0"
+  version             = "1.3.0"
   resource_group_name = "${var.prefix}-resource-group"
 }
 
 ##############################################################################
-# Resource Group 1
+# IBM Cloud Monitoring instance
 ##############################################################################
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version           = "1.2.12"
+  version           = "1.6.0"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
   resource_tags     = var.resource_tags
