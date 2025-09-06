5834e05fcb0c0cb1738786c531f1dfd9e4b8f760
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-09-06 03:59:45 +0000

    fix(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -52,7 +52,7 @@ module "cloud_monitoring" {
 
 module "cbr_schematics_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.33.1"
+  version          = "1.33.2"
   name             = "${var.prefix}-schematics-network-zone"
   zone_description = "CBR Network zone containing Schematics"
   account_id       = module.cloud_monitoring.account_id
