c584604849b921f3e8091dac768c1e96431f7902
ocofaigh <ocofaigh@ie.ibm.com>
2025-09-15 22:32:45 +0100

    fix(deps): update DA dependencies

--- examples/advanced/README.md
+++ examples/advanced/README.md
@@ -2,6 +2,8 @@
 
 Example that configures:
 
-- IBM Cloud Monitoring instance
-- IBM Cloud Metrics Routing
-- A context-based restriction (CBR) rule to only allow cloud monitoring to be accessible from schematics
+- A new resource group if one is not passed in.
+- A context-based restriction (CBR) zone for the IBM Cloud Schematics service.
+- An IBM Cloud Monitoring instance.
+- A context-based restriction (CBR) rule to only allow the Cloud Monitoring to be accessible from the Schematics zone.
+- A Metrics Routing target for the new IBM Cloud Monitoring instance and a route to route send metrics to it.
--- tests/other_test.go
+++ tests/other_test.go
@@ -7,24 +7,21 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
-	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testschematic"
 )
 
 // Ensure every example directory has a corresponding test
 const advancedExampleDir = "examples/advanced"
 const basicExampleDir = "examples/basic"
 
-func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
+func setupExamplesOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
 		Testing:       t,
 		TerraformDir:  dir,
 		Prefix:        prefix,
 		ResourceGroup: resourceGroup,
 		Region:        validRegions[rand.Intn(len(validRegions))],
-		IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
-			List: IgnoreUpdates,
-		},
 	})
+
 	return options
 }
 
@@ -32,42 +29,20 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 func TestRunBasicExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "icm-basic", basicExampleDir)
+	options := setupExamplesOptions(t, "icm-basic", basicExampleDir)
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
 	assert.NotNil(t, output, "Expected some output")
 }
 
-func TestRunAdvancedExampleInSchematics(t *testing.T) {
+// Consistency test for the advanced example
+func TestRunAdvancedExample(t *testing.T) {
 	t.Parallel()
 
-	var region = validRegions[rand.Intn(len(validRegions))]
+	options := setupExamplesOptions(t, "icm-adv", advancedExampleDir)
 
-	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
-		Testing: t,
-		Prefix:  "icm-adv",
-		TarIncludePatterns: []string{
-			"*.tf",
-			"modules/metrics_routing" + "/*.tf",
-			advancedExampleDir + "/*.tf",
-		},
-		ResourceGroup:          resourceGroup,
-		TemplateFolder:         advancedExampleDir,
-		Tags:                   []string{"test-schematic"},
-		DeleteWorkspaceOnFail:  false,
-		WaitJobCompleteMinutes: 60,
-		IgnoreUpdates: testhelper.Exemptions{ // Ignore for consistency check
-			List: IgnoreUpdates,
-		},
-	})
-
-	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
-		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
-		{Name: "prefix", Value: options.Prefix, DataType: "string"},
-		{Name: "region", Value: region, DataType: "string"},
-	}
-
-	err := options.RunSchematicTest()
+	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
 }
