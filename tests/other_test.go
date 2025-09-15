7b357f7f75f81a0ded8e71ac9935db92758deb73
ocofaigh <ocofaigh@ie.ibm.com>
2025-09-15 22:49:19 +0100

    cra update

--- tests/other_test.go
+++ tests/other_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testschematic"
 )
 
 // Ensure every example directory has a corresponding test
@@ -37,12 +38,32 @@ func TestRunBasicExample(t *testing.T) {
 }
 
 // Consistency test for the advanced example
-func TestRunAdvancedExample(t *testing.T) {
+// NOTE: It is run in Schematics because the example configures a CBR rule to only allow traffic from schematics zone
+func TestRunAdvancedExampleInSchematics(t *testing.T) {
 	t.Parallel()
 
-	options := setupExamplesOptions(t, "icm-adv", advancedExampleDir)
+	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
+		Testing: t,
+		Prefix:  "icm-adv",
+		Region:  validRegions[rand.Intn(len(validRegions))],
+		TarIncludePatterns: []string{
+			"*.tf",
+			"modules/metrics_routing" + "/*.tf",
+			advancedExampleDir + "/*.tf",
+		},
+		ResourceGroup:          resourceGroup,
+		TemplateFolder:         advancedExampleDir,
+		Tags:                   tags,
+		DeleteWorkspaceOnFail:  false,
+		WaitJobCompleteMinutes: 60,
+	})
 
-	output, err := options.RunTestConsistency()
+	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
+		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
+		{Name: "prefix", Value: options.Prefix, DataType: "string"},
+		{Name: "region", Value: options.Region, DataType: "string"},
+	}
+
+	err := options.RunSchematicTest()
 	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
 }
