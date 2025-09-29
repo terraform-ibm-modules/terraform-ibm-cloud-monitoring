0807d34d13a280791a8f8425f3b9f86b2463050a
Md Anam Raihan <md.anam.raihan@ibm.com>
2025-09-29 17:39:39 +0530

    resolve comments

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -27,9 +27,9 @@ output "cloud_monitoring_guid" {
   description = "The guid of the provisioned IBM cloud monitoring instance."
 }
 
-output "cloud_monitoring_resource_key" {
+output "cloud_monitoring_resource_keys" {
   value       = local.create_cloud_monitoring ? module.cloud_monitoring[0].resource_keys : null
-  description = "IBM cloud monitoring access key for agents to use"
+  description = "IBM cloud monitoring access keys for agents to use"
   sensitive   = true
 }
 
