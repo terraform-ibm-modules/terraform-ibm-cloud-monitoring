8518cebf26a39f44d5af404699d950a329354c0a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-04 03:56:27 +0000

    fix(deps): update dependencies

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -14,7 +14,7 @@ module "resource_group" {
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version           = "1.8.1"
+  version           = "1.8.2"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
   resource_tags     = var.resource_tags
