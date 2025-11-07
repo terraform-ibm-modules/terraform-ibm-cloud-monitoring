013530ddda0f9bc999883454a99d0060151d7a12
aatreyee257 <mukherjeeaatreyee25@gmail.com>
2025-11-07 18:09:14 +0530

    fixes

--- solutions/metrics-routing-account-settings/outputs.tf
+++ solutions/metrics-routing-account-settings/outputs.tf
@@ -6,28 +6,3 @@ output "metrics_router_account_settings" {
   description = "IBM Cloud metrics router account settings."
   value       = module.metrics_router_account_settings.metrics_router_settings
 }
-
-output "next_steps_text" {
-  value       = "Your Cloud Monitoring Instance is ready."
-  description = "Next steps text"
-}
-
-output "next_step_primary_label" {
-  value       = "Go to Cloud Monitoring Instance"
-  description = "Primary label"
-}
-
-output "next_step_primary_url" {
-  value       = length(module.metrics_router_account_settings.metrics_router_target_crns) > 0 ? "https://cloud.ibm.com/observability/metrics-router/${module.metrics_router_account_settings.metrics_router_target_crns[0]}/overview" : null
-  description = "Primary URL for the IBM Cloud Logs instance"
-}
-
-output "next_step_secondary_label" {
-  value       = "Learn more about Metrics Routing"
-  description = "Secondary label"
-}
-
-output "next_step_secondary_url" {
-  value       = "https://cloud.ibm.com/docs/metrics-router?topic=metrics-router-settings-about&interface=ui"
-  description = "Secondary URL"
-}
