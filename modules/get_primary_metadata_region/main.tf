b3015505617cb5b6135ea1dddb245359fee5046a
Md Anam Raihan <md.anam.raihan@ibm.com>
2025-11-11 14:09:19 +0530

    update main

--- modules/get_primary_metadata_region/main.tf
+++ modules/get_primary_metadata_region/main.tf
@@ -2,25 +2,9 @@
 # IBM Cloud Metric Routing
 #########################################################################
 
-# data "ibm_iam_auth_token" "tokendata" {
-# }
-# 
 resource "time_sleep" "wait_iam_token" {
-  # depends_on      = [data.ibm_iam_auth_token.tokendata]
   create_duration = "5s"
 }
-
-# resource "null_resource" "primary_metadata_region" {
-#   # depends_on = [time_sleep.wait_iam_token]
-#   provisioner "local-exec" {
-#     command = "bash ${path.module}/scripts/get_primary_metadata_region.sh"
-#     environment = {
-#       ibmcloud_api_key     = var.ibmcloud_api_key
-#       REGION               = var.region
-#       USE_PRIVATE_ENDPOINT = var.use_private_endpoint
-#     }
-#   }
-# }
 
 data "external" "get_primary_metadata_region" {
   depends_on = [time_sleep.wait_iam_token]
