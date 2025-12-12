b45cb1f1cf6fb83632a9528979114f979806d4e6
Md Anam Raihan <md.anam.raihan@ibm.com>
2025-12-12 05:51:33 +0530

    resolve comments

--- modules/get_primary_metadata_region/variables.tf
+++ modules/get_primary_metadata_region/variables.tf
@@ -4,6 +4,6 @@
 
 variable "use_private_endpoint" {
   type        = bool
-  description = "Set to true to use the private endpoints instead of public endpoints for IBM Cloud Metrics Routing service. When true, the script queries the private Metrics Routing endpoint for the given region. [Learn more](https://cloud.ibm.com/docs/metrics-router?topic=metrics-router-endpoints)"
+  description = "Set to true to use the private endpoints instead of public endpoints for IBM Cloud Metrics Routing service. When true, the script queries the private Metrics Routing endpoint. [Learn more](https://cloud.ibm.com/docs/metrics-router?topic=metrics-router-endpoints)"
   default     = false
 }
