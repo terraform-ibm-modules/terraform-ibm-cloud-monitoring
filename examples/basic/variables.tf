65b5b1780d2f063011b52455f86109521dfaeab2
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-02-24 02:52:24 +0530

    chore: update tag descriptions and add access_tag dependencies

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -34,6 +34,10 @@ variable "resource_group" {
 
 variable "resource_tags" {
   type        = list(string)
-  description = "List of resource tag to associate with all resource instances created by this example."
+  description = "Add user resource tags to the cloud-monitoring instance to organize, track, and manage costs. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#tag-types)"
+  validation {
+    condition     = alltrue([for tag in var.resource_tags : can(regex("^[A-Za-z0-9 _\\-.:]{1,128}$", tag))])
+    error_message = "Each resource tag must be 128 characters or less and may contain only A-Z, a-z, 0-9, spaces, underscore (_), hyphen (-), period (.), and colon (:)."
+  }
   default     = []
 }
